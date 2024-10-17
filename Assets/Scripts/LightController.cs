6422f965e0c0622d35a95f6fc473bd1009960b16
Serkal <kali.hinder@ultixtechnology.com>
2024-10-17 14:21:24 +0100

    added ability to turn flickering on and off through the inspector

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -1,47 +1,45 @@
 using UnityEngine;
-using System.Collections;
 
 public class LightController : MonoBehaviour
 {
-    // Inspector options
-    [Header("Flicker Settings (select -1 for random flickering)")]
-    [Tooltip("If using Custom, choose a value from the list")]
-    public Light flickeringLight;
-    public float customFlickerInterval = 0.1f;
-
+    private Light flickeringLight;
     private float timer = 0;
     private float flickerInterval;
 
+    [Header("Flicker Settings (input -1 for random flickering)")]
+    [SerializeField] private float customFlickerInterval = 0.1f;
+    [SerializeField] private bool isFlickering = true;
+
     void Start()
     {
-        Debug.Log("We're in Start");
+        flickeringLight = GetComponent<Light>();
     }
 
     void Update()
     {
-        Debug.Log("We're in Update");
-
-        // Light timer interval to cause it to flick at random
-
-        if (customFlickerInterval == -1)
-        {
-            timer += Time.deltaTime;
-            if (timer > flickerInterval)
+        if (isFlickering) 
+        { 
+            // Check if -1 was input for random flickering
+            if (customFlickerInterval == -1)
             {
-                flickeringLight.enabled = !flickeringLight.enabled;
-                flickerInterval = Random.Range(0f, 1f);
-                timer = 0;
+                timer += Time.deltaTime;
+                if (timer > flickerInterval)
+                {
+                    flickeringLight.enabled = !flickeringLight.enabled;
+                    flickerInterval = Random.Range(0f, 1f);
+                    timer = 0;
+                }
+            }
+            // Using custom flicker interval input
+            else
+            {
+                timer += Time.deltaTime;
+                if (timer > customFlickerInterval)
+                {
+                    flickeringLight.enabled = !flickeringLight.enabled;
+                    timer = 0;
+                }
             }
         }
-        else
-        {
-            timer += Time.deltaTime;
-            if (timer > customFlickerInterval)
-            {
-                flickeringLight.enabled = !flickeringLight.enabled;
-                timer = 0;
-            }
-        }
-
     }
 }
