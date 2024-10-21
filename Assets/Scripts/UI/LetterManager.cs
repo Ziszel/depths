d4e6c7db179d5ed874353573332a35a3d70b562e
Serkal <kali.hinder@ultixtechnology.com>
2024-10-21 22:26:12 +0100

    new letter prefab, can press E to open and exit

--- Assets/Scripts/UI/LetterManager.cs
+++ Assets/Scripts/UI/LetterManager.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LetterManager : MonoBehaviour
+public class LetterManager : MonoBehaviour, IInteractable
 {
-    // edit
     public Button _letterExitBtn;
     public Button _letterContinueBtn;
 
+    private PlayerController _player;
+
     void Start()
     {
+        _player = FindAnyObjectByType<PlayerController>();
         _letterExitBtn.onClick.AddListener(OnLetterExitClicked);
         _letterContinueBtn.onClick.AddListener(OnLetterContinueClicked);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _player.SetCurrentInteractable(this.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _player.SetCurrentInteractable(null);
+        }
     }
 
     private void OnLetterExitClicked()
@@ -22,5 +40,19 @@ public class LetterManager : MonoBehaviour
     {
         // call game manager to deactivate this 
         GameManager.instance.LetterContinue();
+    }
+
+    public void AttemptToInteract()
+    {
+        
+        if (GameManager.instance.IsLetterActive())
+        {
+            GameManager.instance.DeactivateLetter();
+        }
+        else
+        {
+            GameManager.instance.ActivateLetter();
+        }
+        // Figure out a way to hide buttons (or hide one and edit text of other) if we're not in initial starting letter
     }
 }
