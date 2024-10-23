a1407c4d2b7a7b4943283159986e3211d0f9de7e
Serkal <kali.hinder@ultixtechnology.com>
2024-10-23 11:50:49 +0100

    added field so we can change the text of the lettr - may be useful

--- Assets/Scripts/UI/LetterManager.cs
+++ Assets/Scripts/UI/LetterManager.cs
@@ -1,18 +1,29 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LetterManager : MonoBehaviour, IInteractable
 {
-    public Button _letterExitBtn;
-    public Button _letterContinueBtn;
+    [SerializeField] private Button _letterExitBtn;
+    [SerializeField] private Button _letterContinueBtn;
+
+    [Header("Leave empty if you want to use the text in the TMP Object inside LetterCanvas")]
+    [SerializeField] private string _letterContents;
 
     private PlayerController _player;
+    private TextMeshPro _textMeshPro;
 
     void Start()
     {
         _player = FindAnyObjectByType<PlayerController>();
         _letterExitBtn.onClick.AddListener(OnLetterExitClicked);
         _letterContinueBtn.onClick.AddListener(OnLetterContinueClicked);
+
+        _textMeshPro = transform.GetChild(0).GetComponentInChildren<TextMeshPro>();
+        if (_letterContents.Length != 0)
+        {
+            _textMeshPro.SetText(_letterContents);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
