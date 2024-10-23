d2e42206bf60405b7a40d30213f63f7397492756
Serkal <kali.hinder@ultixtechnology.com>
2024-10-23 11:29:59 +0100

    letter music now plays on screen for all letters

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -77,33 +77,26 @@ public class GameManager : MonoBehaviour
         }
         else // We're in a game level or testing level
         {
-            // Show letter, and initially pause game whilst user reads the letter (unpauses on letter continue)
             _letterCanvas = GameObject.Find("LetterCanvas");
-            ActivateLetter();
-
-            // Start the game level with a black screen, fading into the letter screen
-            _blackFadeCanvas = GameObject.Find("BlackFadeCanvas");
-            _blackFadeCanvas.SetActive(true);
-            blackFadeTransition = _blackFadeCanvas.GetComponentInChildren<BlackFadeTransition>();
-            blackFadeTransition.TriggerFadeFromBlack(null);
-
-            // Play initial intro music for the letter
             _musicManager = GameObject.Find("MusicAudioSource").GetComponentInChildren<MusicManager>();
-            _musicManager.Play();
+            _blackFadeCanvas = GameObject.Find("BlackFadeCanvas");
+            monster = GameObject.Find("Monster").GetComponentInChildren<Monster>(); // Get the monster stored so we're able to play chasing/wandering music
+            if (monster != null)
+            {
+                monster.OnChaseStateEntered += HandleMonsterEnterChaseState;
+                monster.OnChaseStateExited += HandleMonsterExitChaseState;
+            }
 
-            // Adjust button layout of options menu for game level
+            // Adjust options menu layout for game level
             RectTransform rectTransform = _mainMenuBtn.GetComponent<RectTransform>();
             rectTransform.anchoredPosition = new Vector3(146, -323, 0);
             _resumeBtn.SetActive(true);
 
-            // Get the monster stored so we're able to play chasing/wandering music
-            monster = GameObject.Find("Monster").GetComponentInChildren<Monster>();
-            if (monster != null)
-            {
-                // Subscribe to the monster's OnChaseStateEntered event
-                monster.OnChaseStateEntered += HandleMonsterEnterChaseState;
-                monster.OnChaseStateExited += HandleMonsterExitChaseState;
-            }
+            // First we start game level with a black screen, fading into the letter screen, with music
+            ActivateLetter();
+            _blackFadeCanvas.SetActive(true);
+            blackFadeTransition = _blackFadeCanvas.GetComponentInChildren<BlackFadeTransition>();
+            blackFadeTransition.TriggerFadeFromBlack(null);
         }
     }
 
@@ -189,14 +182,7 @@ public class GameManager : MonoBehaviour
         _blackFadeCanvas.SetActive(true);
         blackFadeTransition.TriggerFadeToBlackAndBack(DeactivateLetter, null); // This will call DeactivateBlackFade() once fade is complete
 
-        // Deactivate the cursor when beginning the game after reading the letter
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-        _musicManager.TriggerFadeOutMusic();
-
-        // Ensure game is unpaused after user reads letter
-        Time.timeScale = 1f;
+        DeactivateLetter();
     }
 
     public void DeactivateBlackFade()
@@ -209,14 +195,24 @@ public class GameManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         Time.timeScale = 0f;
+
+        _musicManager.AssignIntroMusic();
+        _musicManager.Play();
+
         _letterCanvas.SetActive(true);
     }
 
     public void DeactivateLetter()
     {
+        // Deactivate the cursor when resuming the game after reading a letter
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        Time.timeScale = 1f;
+
+        // Ensure game is unpaused after user reads letter
+        Time.timeScale = 1f; 
+
+        _musicManager.TriggerFadeOutMusic();
+
         _letterCanvas.SetActive(false);
     }
 
